0045bdef03779db18b9d733b4bbb924ce41e91db
Niklas Debbrecht <niklas@debbrecht.com>
2017-12-18 16:05:23 +0100

    added column

--- Entity/Ticket.php
+++ Entity/Ticket.php
@@ -25,6 +25,13 @@ class Ticket
     protected $id;
 
     /**
+    * @var string
+    *
+    * @ORM\Column(name="salesforceId", type="string", length=255)
+    **/
+    protected $salesforceId;
+
+    /**
      * @var integer
      *
      * @ORM\Column(name="user_created_id", type="guid")
@@ -375,4 +382,27 @@ class Ticket
     {
         return $this->messages;
     }
+
+    /**
+     * Set salesforceId
+     *
+     * @param string $salesforceId
+     * @return Ticket
+     */
+     public function setSalesforceId($salesforceId)
+     {
+         $this->salesforceId = $salesforceId;
+
+         return $this;
+     }
+
+     /**
+      * Get salesforceId
+      *
+      * @return string Salesforce ID
+      */
+     public function getSalesforceId()
+     {
+         return $this->salesforceId;
+     }
 }
